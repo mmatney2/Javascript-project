1c2468e6a92ac61ede19f9e8650b0d4cebe32a3e
Marquita Matney <marquita.matney@gmail.com>
2022-09-02 13:45:42 -0400

    first commit

---
+++ jsProject.js
@@ -0,0 +1,24 @@
+// let, const, var
+//you can change const but you can't reassign 
+let num = 5;
+console.log(num)
+// num = 6;
+// console.log(num)
+// const arr = [1,2,3];
+// arr.push(4)
+// arr[0] = 0
+// console.log(arr)
+
+function f() {
+    console.log('this is f')
+}
+
+const d = (a, b) => {return a+b}
+//or
+const d1 = () => {
+    return console.log('maybe, maybe not')
+};
+
+// const plusOne = function f  = ((a)=>{
+//     console.log('l')
+// })
